f4023e78344a91289c62a6458c3a40ba12fd5709
Clément HELIOU <cheliou@xebia.fr>
2015-02-09 21:46:24 +0100

    feat(fizzbuzz): add FizzBuzz variant

--- src/main/scala/fr/xebia/kata/fizzbuzz/scala/FizzBuzz.scala
+++ src/main/scala/fr/xebia/kata/fizzbuzz/scala/FizzBuzz.scala
@@ -2,7 +2,13 @@ package fr.xebia.kata.fizzbuzz.scala
 
 object FizzBuzz {
 
-  def computeFizzBuzz(): String = throw new UnsupportedOperationException()
+  def computeFizzBuzz(): String = {
+    (1 to 100) map { case i if i % 15 == 0 => "FizzBuzz"
+    case i if i % 5 == 0 => "Buzz"
+    case i if i % 3 == 0 => "Fizz"
+    case i => i toString
+    } mkString ", "
+  }
 
   def computeFizzBuzzWoof(): String = throw new UnsupportedOperationException()
 }
